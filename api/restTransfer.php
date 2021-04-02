cbbcb6c78f68346df8c79f6a0c25a68e8c1bf3a0
Lucas Juan Souza de Oliveira Miranda Moraes <miscrits.lucas@gmail.com>
2021-04-02 12:01:55 -0300

    ajstes finais

--- api/restTransfer.php
+++ api/restTransfer.php
@@ -2,5 +2,5 @@
     include "../classes/transfer.php";
     include "../classes/account.php";
     date_default_timezone_set("America/Sao_Paulo");
-    $transfer = new Transfer(new Account(51074986604), new Account(12312312312), date("d/m/y h:i:s"), 1500);
+    $transfer = new Transfer(new Account($_POST["origem"]), new Account($_POST["destino"]), date("d/m/y h:i:s"), $_POST["valor"]);
     echo json_encode($transfer->transferencia());
